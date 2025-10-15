b6db96faca6c76bd1052e93e15318643c6bc3bee
Lucas emanoel <lucasemanoel625@gmail.com>
2025-10-15 20:50:07 -0300

    feat:  Cria a lógica de cadastro de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
-import { Router, type Request, type Response } from 'express';
+import { Router } from 'express';
+
+import { CreateUserController } from './controllers/user/createUserController.js';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({name: 'Fast food'})
-})
+router.post('/users', new CreateUserController().handle)
 
 export {router};
---
+++ src/services/user/createUserService.ts
@@ -0,0 +1,44 @@
+import prismaClient from '../../prisma/index.js'
+
+interface UserRequest{
+    name: string;
+    email: string;
+    password: string
+}
+
+class CreateUserService{
+    async execute({name, email, password}: UserRequest){
+
+        if(!email){
+            throw new Error("Email incorrect")
+        }
+
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error("User already exists")
+        }
+
+        const user = await prismaClient.user.create({
+            data: {
+               name: name,
+               email: email,
+               password: password, 
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+
+
+        return user;
+    }
+}
+
+export {CreateUserService}
