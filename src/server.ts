d2f8792a5e4773393640e33fd1b038a572a0c758
Lucas emanoel <lucasemanoel625@gmail.com>
2025-10-14 21:13:28 -0300

    feat: Integração Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, { type Request, type Response, type NextFunction} from 'express';
+import cors from 'cors';
+
 import {router} from './routes.js'
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use(router)
+app.use(router);
 
 app.listen(3333, () => console.log("Servidor online!"))
