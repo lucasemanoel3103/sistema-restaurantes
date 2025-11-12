5ef684eda10ed064e43452bcd82a9bddbbf2bcdf
Lucas emanoel <lucasemanoel625@gmail.com>
2025-11-12 20:07:22 -0300

    feat: Lógica de logout da conta

--- frontend/src/app/dashboard/components/header/index.tsx
+++ frontend/src/app/dashboard/components/header/index.tsx
@@ -1,10 +1,22 @@
+"use client"
+
 import styles from './styles.module.scss'
 import Link from 'next/link'
 import Image from 'next/image'
 import logoImg from '/public/pedidoflow.svg'
 import { LogOutIcon } from 'lucide-react'
+import { deleteCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
 
 export function Header(){
+    const router = useRouter();
+
+    async function handleLogout(){
+        deleteCookie("session", {path: "/"})
+
+        router.replace("/")
+    }
+
     return(
        <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
@@ -27,7 +39,7 @@ export function Header(){
                         Produto
                     </Link>
 
-                    <form>
+                    <form action={handleLogout}>
                         <button type='submit'>
                            <LogOutIcon size={24} color='#fff'/> 
                         </button>
