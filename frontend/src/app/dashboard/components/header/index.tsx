1edb2da8fb02e6aeb03526d254e497a59b82311e
Lucas emanoel <lucasemanoel625@gmail.com>
2025-11-12 19:50:23 -0300

    feat: Cria o header da página

---
+++ frontend/src/app/dashboard/components/header/index.tsx
@@ -0,0 +1,39 @@
+import styles from './styles.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+import logoImg from '/public/pedidoflow.svg'
+import { LogOutIcon } from 'lucide-react'
+
+export function Header(){
+    return(
+       <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Link href="/dashboard">
+                    <Image
+                        alt='Logo Pedido Flow'
+                        src={logoImg}
+                        width={190}
+                        height={60}
+                        priority={true}
+                        quality={100}
+                    />
+                </Link>
+
+                <nav>
+                    <Link href="/dashboard/category">
+                        Categoria
+                    </Link>""
+                    <Link href="/dashboard/product">
+                        Produto
+                    </Link>
+
+                    <form>
+                        <button type='submit'>
+                           <LogOutIcon size={24} color='#fff'/> 
+                        </button>
+                    </form>
+                </nav>
+            </div>    
+       </header>    
+    )
+}
