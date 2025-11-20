ef9b4356aebbd989fd0f35a09e5ce1b952c0f63e
Lucas emanoel <lucasemanoel625@gmail.com>
2025-11-20 15:09:41 -0300

    feat: Cria a página de cadastro de produtos

---
+++ frontend/src/app/dashboard/category/page.tsx
@@ -0,0 +1,53 @@
+import styles from './styles.module.scss'
+import { Button } from '../components/button'
+import { api } from '@/services/api'
+import { getCookieServer } from '@/lib/cookieServers'
+import { redirect } from 'next/navigation'
+
+export default function Category(){
+
+    async function handleRegisterCategory(formData: FormData){
+        "use server"
+        
+        const name = formData.get("name")
+
+        if(!name) return;
+
+        const data = {
+            name: name
+        }
+
+        const token = await getCookieServer()
+
+        await api.post("/create", data, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            return;
+        })
+
+        redirect("/dashboard")
+    }
+
+    return(
+        <main className={styles.container}>
+            <h1>Nova categoria</h1>
+
+
+            <form className={styles.form} action={handleRegisterCategory}>
+                <input 
+                type="text" 
+                name="name"
+                placeholder="Nome da categoria, ex: Lanches"
+                required
+                className={styles.input}
+                />
+
+                <Button name='Cadastrar'/>
+            </form>
+        </main>
+    )
+}
